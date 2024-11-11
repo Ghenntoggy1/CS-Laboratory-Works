869fc23b6bcec4cb2d06d6525f48089f931b0285
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-11-12 01:54:08 +0200

    feature: Implement DES Encryption. Add S_BOXES, P_BOX, IP_INVERSE matrices to constants.py.

--- Laboratory_Work_4_DES/fastAPI/awf.py
+++ Laboratory_Work_4_DES/fastAPI/awf.py
@@ -1,43 +1,73 @@
-from Laboratory_Work_4_DES.fastAPI.constants import PC_1, IP, PC_2, E_BIT_SELECTION
+from Laboratory_Work_4_DES.fastAPI.constants import PC_1, IP, PC_2, E_BIT_SELECTION, IP_INVERSE
 from Laboratory_Work_4_DES.fastAPI.main import initial_permutation_key, split_key, compile_keys, permute_key, \
-    permute_message, split_message
+    permute_message, split_message, function_f, compile_message_portions
 
-plain_text: str = "0123456789ABCDEF"
+print("STEP 0: PREREQUISITES")
 plain_text: str = "0123456789ABCDEF"
 print(f"Received Message: {plain_text}")
 key: str = "133457799BBCDFF1"
 print(f"Received Key: {key}")
 
+print("STEP 1: CONVERT HEXADECIMAL TO BINARY")
 binary_string = bin(int(plain_text, 16))[2:].zfill(len(plain_text) * 4)
 print(f"Convert Message from Hexadecimal to Binary : {binary_string}")
-
 binary_key = bin(int(key, 16))[2:].zfill(len(key) * 4)
 print(f"Convert Key from Hexadecimal to Binary : {binary_key}")
 
+print("STEP 2: SPLIT MESSAGE")
 L, R = split_message(binary_string)
 print(f"Split Message into L and R : {L}, {R}")
 
+print("STEP 3: INITIAL PERMUTATION OF KEY")
 permuted_key: str = initial_permutation_key(binary_key)
 print(f"Initial Permutation of Key : {permuted_key}")
 print(f"PC-1 :\n{PC_1}")
 
+print("STEP 4: SPLIT KEY")
 C_0, D_0 = split_key(permuted_key)
 print(f"Split Key into C_0 and D_0 : {C_0}, {D_0}")
 
-keys: list[str] = compile_keys(C_0, D_0, 16)
-print(f"Compile Keys :\n{"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(keys, start=1))}")
+print("STEP 5: COMPILE ROUND KEYS")
+permuted_keys: list[str] = compile_keys(C_0, D_0, 16)
+# print(f"Compile Keys :\n{"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(permuted_keys, start=1))}")
 
-permuted_keys: list[str] = [permute_key(key) for key in keys]
-print(f"Permuted Keys :\n{"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(keys, start=1))}")
-print(f"PC-2 :\n{PC_2}")
+# print("STEP 6: PERMUTE KEYS")
+# permuted_keys: list[str] = [permute_key(key) for key in keys]
+# print(f"Permuted Keys :\n{"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(permuted_keys, start=1))}")
+# print(f"PC-2 :\n{PC_2}")
 
+print("STEP 7: INITIAL PERMUTATION OF MESSAGE")
 permuted_message: str = permute_message(binary_string, IP)
 print(f"Initial Permutation of Message : {permuted_message}")
 print(f"IP :\n{IP}")
 
+print("STEP 8: SPLIT MESSAGE")
 L_0, R_0 = split_message(permuted_message)
 print(f"Split Permuted Message into L_0 and R_0 : {L_0}, {R_0}")
-permuted_R = permute_message(R_0, E_BIT_SELECTION)
+expanded_R = permute_message(R_0, E_BIT_SELECTION)
 
-print(f"Permuted R_0 : {permuted_R}")
+print("STEP 9: EXPAND R_0")
+print(f"Expanded R_0 : {expanded_R}")
 print(f"E-Bit Selection :\n{E_BIT_SELECTION}")
+
+# print("STEP 10: FUNCTION F")
+# print(f"f(R_(n-1), K_n) = {function_f(R_0, permuted_keys[0])}")
+
+print("STEP 10: COMPILE MESSAGE PORTIONS")
+messages_list = compile_message_portions(L_0, R_0, permuted_keys, 16)
+
+print(f"Message Portions:\n{"\n".join(f"L_{message_nr} : {L}, R_{message_nr} : {R}" for message_nr, (L, R) in enumerate(messages_list, start=1))}")
+
+print("STEP 11: REVERSE MESSAGE")
+L_16, R_16 = messages_list[-1]
+reversed_message = R_16 + L_16
+print(f"Reversed Message : {reversed_message}")
+
+print("STEP 12: FINAL PERMUTATION")
+final_permutation = permute_message(reversed_message, IP_INVERSE)
+print(f"Final Permutation : {final_permutation}")
+print(f"IP-1 :\n{IP_INVERSE}")
+
+print("STEP 13: CONVERT BINARY TO HEXADECIMAL")
+hexadecimal_string = hex(int(final_permutation, 2))[2:].upper()
+print(f"Convert Message from Binary to Hexadecimal : {hexadecimal_string}")
--- Laboratory_Work_4_DES/fastAPI/main.py
+++ Laboratory_Work_4_DES/fastAPI/main.py
@@ -1,9 +1,11 @@
 import numpy
 import numpy as np
 from fastapi import FastAPI, status
+from numpy.ma.core import right_shift
 from pydantic import BaseModel
 from numpy import mod
 
+from Laboratory_Work_4_DES.fastAPI.constants import list_of_S_BOXES, P_BOX, IP_INVERSE
 from constants import PC_1, PC_2, IP, E_BIT_SELECTION
 
 app = FastAPI()
@@ -44,11 +46,7 @@ def encrypt(request_body: RequestBody):
     C_0, D_0 = split_key(permuted_key)
     print(f"Split Key into C_0 and D_0 : {C_0}, {D_0}")
 
-    keys: list[str] = compile_keys(C_0, D_0, 16)
-    print(f"Compile Keys : {"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(keys, start=1))}")
-
-    permuted_keys: list[str] = [permute_key(key) for key in keys]
-    print(f"Permuted Keys : {"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(keys, start=1))}")
+    permuted_keys: list[str] = compile_keys(C_0, D_0, 16)
 
     permuted_message: str = permute_message(binary_string, IP)
     print(f"Initial Permutation of Message : {permuted_message}")
@@ -58,21 +56,89 @@ def encrypt(request_body: RequestBody):
 
     expanded_R = permute_message(R_0, E_BIT_SELECTION)
     print(f"Permuted R_0 : {expanded_R}")
+    print(f"E-Bit Selection :\n{E_BIT_SELECTION}")
 
+    messages_list = compile_message_portions(L_0, R_0, permuted_keys, 16)
+    print(f"Messages List : {"\n".join(f"Key {message_nr} : {message}" for message_nr, message in enumerate(messages_list, start=1))}")
 
+    L_16, R_16 = messages_list[-1]
+    reversed_message = R_16 + L_16
+    print(f"Reversed Message : {reversed_message}")
 
+    final_permutation = permute_message(reversed_message, IP_INVERSE)
+    print(f"Final Permutation : {final_permutation}")
+    print(f"IP-1 :\n{IP_INVERSE}")
 
-def compile_keys(C_0: str, D_0: str, round: int = 16) -> list[str]:
-    keys: list[str] = [C_0+D_0]
+    hexadecimal_string = hex(int(final_permutation, 2))[2:].upper()
+    print(f"Convert Message from Binary to Hexadecimal : {hexadecimal_string}")
+
+    return Response(
+        content={"message": "Encryption Successful",
+                 "encrypted_message": hexadecimal_string,
+                 "status_code": status.HTTP_200_OK},
+        status_code=status.HTTP_200_OK
+    )
+
+def compile_message_portions(L_0: str, R_0: str, permuted_keys: list[str], round: int = 16) -> list[tuple[str, str]]:
+    message_portions: list[tuple[str, str]] = [(L_0, R_0)]
     round = min(max(round, 1), 16)
     for i in range(0, round):
-        # TODO: implement right shift
-        left_shift = 1 if i + 1 in [1, 2, 9, 16] else 2
-        C_i = C_0[left_shift:] + C_0[:left_shift]
-        D_i = D_0[left_shift:] + D_0[:left_shift]
-        C_0, D_0 = C_i, D_i
-        keys.append(C_i + D_i)
-    return keys[1:]
+        K_i = permuted_keys[i]
+        L_i = R_0
+        R_i = bin(int(L_0, 2) ^ int(function_f(R_0, K_i), 2))[2:].zfill(len(L_0))
+        L_0, R_0 = L_i, R_i
+        message_portions.append((L_i, R_i))
+
+    return message_portions[1:]
+
+def function_f(R_0: str, K_1: str) -> str:
+    S_BOXED_R: str = ""
+
+    expanded_R_0 = permute_message(R_0, E_BIT_SELECTION)
+    print(f"Expanded R_0 : {expanded_R_0}")
+    print(f"E-Bit Selection :\n{E_BIT_SELECTION}")
+
+    expanded_XORed_R_0 = bin(int(expanded_R_0, 2) ^ int(K_1, 2))[2:].zfill(len(expanded_R_0))
+    print(f"R_0 XOR K_1 = {expanded_XORed_R_0}")
+    for block_nr, block_length in enumerate(range(0, len(expanded_XORed_R_0), 6), start=0):
+        block = expanded_XORed_R_0[block_length:block_length+6]
+        print(f"Block {block_nr + 1} : {block}")
+        row = int(block[0] + block[-1], 2)
+        column = int(block[1:-1], 2)
+        print(f"Row : {row}, Column : {column}")
+        print(f"S{block_nr + 1}-Box :\n{list_of_S_BOXES[block_nr]}")
+        print(f"S{block_nr + 1}-Box Value : {list_of_S_BOXES[block_nr][row][column]}")
+        S_BOXED_R += bin(list_of_S_BOXES[block_nr][row][column])[2:].zfill(4)
+    print(f"S-Boxed R : {S_BOXED_R}")
+    permuted_S_BOXED_R = permute_message(S_BOXED_R, P_BOX)
+    print(f"Result R : {permuted_S_BOXED_R}")
+    print(f"P-Box :\n{P_BOX}")
+
+    return permuted_S_BOXED_R
+
+def compile_keys(C_0: str, D_0: str, round: int = 16, is_left_shift: bool = True) -> list[str]:
+    keys: list[str] = [C_0+D_0]
+    round = min(max(round, 1), 16)
+
+    if is_left_shift:
+        for i in range(0, round):
+            left_shift = 1 if i + 1 in [1, 2, 9, 16] else 2
+            C_i = C_0[left_shift:] + C_0[:left_shift]
+            D_i = D_0[left_shift:] + D_0[:left_shift]
+            C_0, D_0 = C_i, D_i
+            keys.append(C_i + D_i)
+    else:
+
+        for i in range(0, round):
+            right_shift = 1 if i + 1 in [2, 9, 16] else 2
+            C_i = C_0[right_shift:] + C_0[:right_shift]
+            D_i = D_0[right_shift:] + D_0[:right_shift]
+            C_0, D_0 = C_i, D_i
+            keys.append(C_i + D_i)
+    permuted_keys: list[str] = [permute_key(key) for key in keys[1:]]
+    print(f"{len(permuted_keys)}")
+    print(f"Round Keys :\n{"\n".join(f"Key {key_nr} : {key}" for key_nr, key in enumerate(permuted_keys, start=1))}")
+    return permuted_keys
 
 def split_message(message: str) -> tuple[str, str]:
     return message[:len(message) // 2], message[len(message) // 2:]
